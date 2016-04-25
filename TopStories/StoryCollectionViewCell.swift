abd3624b6cbbd5cb546e2c67e08632090980e673
Sam Meadley <sam.meadley@gmail.com>
2016-04-25 11:08:26 +0100

    Hide cell imageView if no image available for story

--- TopStories/StoryCollectionViewCell.swift
+++ TopStories/StoryCollectionViewCell.swift
@@ -11,19 +11,28 @@ import UIKit
 class StoryCollectionViewCell: UICollectionViewCell {
 
     static let defaultReuseIdentifier = String(StoryCollectionViewCell)
-    
+        
     var image: UIImage? {
         didSet {
             imageView?.image = image
         }
     }
     
+    private let defaultImageViewWidth: CGFloat = 75
+    
     @IBOutlet private weak var titleLabel: UILabel?
     @IBOutlet private weak var imageView: UIImageView?
+    @IBOutlet private weak var imageViewWidthConstraint: NSLayoutConstraint?
 
     func updateForStory(story: Story) {
         
         titleLabel?.text = story.title
+        
+        if story.imageURLForSize(Story.ImageSize.Thumbnail) == nil {
+            imageViewWidthConstraint?.constant = 0
+        } else {
+            imageViewWidthConstraint?.constant = defaultImageViewWidth
+        }
     }
     
     // MARK: - UICollectionViewCell
